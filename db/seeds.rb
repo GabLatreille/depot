1bc5432db9d6ac92c94bab9b2240e17377b524c6
Gabrielle Latreille <gabrielle.latreille@shopify.com>
2018-07-05 20:30:24 -0400

    Task A2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+#...
+Product.create!(title: 'Product 1',
+  description:
+    %{<p>
+      <em>This is the first product.
+      </p>},
+  image_url: 'http://tarotnumerologiczny.pl/wp-content/uploads/Wibracja-numerologiczna-liczby-1..jpg',
+price: 1.00)
+#...
+Product.create!(title: 'Product 2',
+  description:
+    %{<p>
+      <em>This might be the second product
+      </p>},
+  image_url: 'http://veastrology.com/images/num2.jpg',
+price: 2.00)
+#...
+Product.create!(title: 'Product 3',
+  description:
+    %{<p>
+      <em>This could very well be the third product
+      </p>},
+  image_url: 'https://imgc.allpostersimages.com/img/print/posters/number-3-birthday-party-lifesize-standup_a-G-5030447-0.jpg',
+price: 3.00)
